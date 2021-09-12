0eeaf4f307a1e86162438adb64c9990cbbb19a62
DubininEvgeny1972 <duba1972@yandex.ru>
2021-09-12 19:09:03 +0300

    Коннектим к БД Util 2

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -21,8 +25,14 @@ public class UserServiceImpl implements UserService {
 
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List<User> user = new ArrayList<>();
+        ResultSet rs = Util.statement.executeQuery("SELECT * FROM USER;");
+        while(rs.next()) {
+            User us = new User(rs.getString(1), rs.getString(2), rs.getByte(3));
+            user.add(us);
+        }
+        return user;
     }
 
     public void cleanUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,28 +1,47 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
 
 public class Util {
-    public static final String USER_NAME = "root";
-    public static final String PASSWORD = "root";
+//    public static final String USER_NAME = "root";
+//    public static final String PASSWORD = "root";
     public static final String URL = "jdbc:mysql://localhost:3306/mysql";
     public static Statement statement;
     public static Connection connection;
-    static {
-        try {
-            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            throw new RuntimeException();
-        }
-        try {
-            statement = connection.createStatement();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            throw new RuntimeException();
-        }
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public Statement getConnection(){
+
+            Connection connection;
+            try {
+                Driver driver = new FabricMySQLDriver();
+                DriverManager.registerDriver(driver);
+
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+                if (!connection.isClosed()) {
+                    System.out.println("Соединение с БД установлено");
+                }
+            }catch (SQLException e) {
+                System.out.println("Не удалось загрузить класс драйвера");
+            }
+//        try {
+//            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//
+//            }
+//        try {
+//                statement = connection.createStatement();
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//
+//            }
+        return statement;
     }
+
+
 }
